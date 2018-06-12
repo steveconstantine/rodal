f71765d7cc91119dc28f92871112056f3395eb59
steveconstantine <38266065+steveconstantine@users.noreply.github.com>
2018-06-11 17:43:58 -0700

    Update rodal.js

--- src/rodal.js
+++ src/rodal.js
@@ -136,6 +136,7 @@ class Rodal extends React.Component {
         return (
             <div
                 style={style}
+                id={'rodal'}
                 className={"rodal rodal-fade-" + state.animationType + ' ' + props.className}
                 onAnimationEnd={this.animationEnd}
                 tabIndex="-1"
